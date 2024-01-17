cad4201f798e852974aa5efba3353377a6bea1ce
Martin Jefimov <martinjefimov12@gmail.com>
2024-01-17 16:34:00 +0000

    added media queries and a sticky header with JS functionality

---
+++ Pratice Projects/First after months/JS/header.js
@@ -0,0 +1,12 @@
+window.onscroll = function () { activateSticky() };
+
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+
+function activateSticky() {
+  if (window.scrollY > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
