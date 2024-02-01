6f315480ab963d5d6d5b31405cdbb7030b2ab60f
Martin Jefimov <martinjefimov12@gmail.com>
2024-02-01 18:22:58 +0000

    Updated advanced animated dropwdown code with latest improvements.

--- Pratice Projects/Advanced_Animated_Dropdown_Menu_Prototype/main.js
+++ Pratice Projects/Advanced_Animated_Dropdown_Menu_Prototype/main.js
@@ -3,6 +3,7 @@ var content = document.getElementsByClassName('content')[0];
 var child = document.getElementsByClassName('child')[0];
 var numChildren = document.getElementsByClassName('child').length;
 var contentHeight;
+var contentWidth;
 
 // On DOMCOntentLoaded Gather information about chidren and the the contentHeight for the dropdown. 
 // Future improvements: Use cookies to store the contentHeight so this doesn't have to be executed every time the page is refreshed.
@@ -14,6 +15,11 @@ window.addEventListener('DOMContentLoaded', (e) =>{
 
 dropDown.addEventListener('mouseover', (e) => {
   content.style.display = 'block';
+
+  var computedHeight = getComputedStyle(content).getPropertyValue('--content-height');
+  if (computedHeight == `${contentHeight}px`){
+    return;
+  }
   content.style.setProperty('--content-height', `${contentHeight}px`);
 });
 
