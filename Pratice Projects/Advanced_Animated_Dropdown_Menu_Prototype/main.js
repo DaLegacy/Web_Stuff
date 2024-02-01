21f89807d16f4d0666a8da2bd66408fc0afddbf6
Martin Jefimov <martinjefimov12@gmail.com>
2024-02-01 15:54:56 +0000

    Created the actual logic for the dropdown menu on hover using JS and dynamic css variable updating

---
+++ Pratice Projects/Advanced_Animated_Dropdown_Menu_Prototype/main.js
@@ -0,0 +1,24 @@
+var dropDown = document.getElementsByClassName('dropdown')[0];
+var content = document.getElementsByClassName('content')[0];
+var child = document.getElementsByClassName('child')[0];
+var numChildren = document.getElementsByClassName('child').length;
+var contentHeight;
+
+// On DOMCOntentLoaded Gather information about chidren and the the contentHeight for the dropdown. 
+// Future improvements: Use cookies to store the contentHeight so this doesn't have to be executed every time the page is refreshed.
+window.addEventListener('DOMContentLoaded', (e) =>{
+  content.style.display = 'block';
+  contentHeight = child.offsetHeight * numChildren;
+  content.style.display = 'none';
+});
+
+dropDown.addEventListener('mouseover', (e) => {
+  content.style.display = 'block';
+  content.style.setProperty('--content-height', `${contentHeight}px`);
+});
+
+dropDown.addEventListener('mouseleave', (e) => {
+  content.style.display = 'none';
+});
+
+
