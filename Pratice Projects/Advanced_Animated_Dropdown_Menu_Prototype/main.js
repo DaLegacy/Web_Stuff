4e847a73ff6ff9c8d30d7160231426725d58b198
Martin Jefimov <martinjefimov12@gmail.com>
2024-02-02 18:27:16 +0000

    (Look IF NEEDED) Removed stack overflow comments to transfer code to main page and updated HTML to more prototype kind

--- Pratice Projects/Advanced_Animated_Dropdown_Menu_Prototype/main.js
+++ Pratice Projects/Advanced_Animated_Dropdown_Menu_Prototype/main.js
@@ -4,10 +4,6 @@ const child = document.getElementsByClassName('child')[0];
 const numChildren = document.getElementsByClassName('child').length;
 var contentHeight;
 
-// On DOMCOntentLoaded Gather information about chidren and the the contentHeight for the dropdown. This can be improved to work with a much better updating time instead of re-loading the page all the time if things change within the variables. (Thing for later)
-
-// Future improvements: Use cookies to store the contentHeight so this doesn't have to be executed every time the page is refreshed. Add if statement if the height is available and return;
-
 window.addEventListener('DOMContentLoaded', (e) =>{
   content.style.display = 'block';
   contentHeight = child.offsetHeight * numChildren;
