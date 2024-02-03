d736155d15788f5f035afb7a79a041fc3225d203
Martin Jefimov <martinjefimov12@gmail.com>
2024-02-03 00:18:18 +0000

    Updated and abstracted animations from elements

---
+++ Pratice Projects/Portfolio/JS/Animation_Listeners/bounce_animation_listener.js
@@ -0,0 +1,14 @@
+// const localRootURL = "http://127.0.0.1:5500/Pratice%20Projects/Portfolio/HTML/";
+// const localRootURLToTestPage = `${localRootURL}testPage.html`;
+
+// // All elements using bounce animation
+// const t = document.getElementById("testing");
+
+// t.addEventListener("click", (e) => {
+//   t.classList.add("bounce");
+// });
+
+// t.addEventListener("animationend", (e) => {
+//   t.classList.remove("bounce");
+//   window.location.href = localRootURLToTestPage;
+// });
